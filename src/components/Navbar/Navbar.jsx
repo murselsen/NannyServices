84fc886024f706d320696142e6e605f24de89238
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-11-29 00:12:32 +0300

    Navbar bileşeninin yapısı güncellendi, gereksiz stil ve bileşenler kaldırıldı.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,7 @@
 import Css from "./Navbar.module.css";
 
-// Component for selecting theme (light/dark)
-import ThemeSelector from "../ThemeSelector/ThemeSelector";
-
-const Navbar = () => {
-  return (
-    <nav className={Css.Navbar}>
-      <div className={Css.Logo}>Nanny.Services</div>
-      
-      <div className={Css.AuthButtons}>
-        <button className={Css.LoginButton}>Login</button>
-        <button className={Css.SignupButton}>Registration</button>
-        <ThemeSelector />
-      </div>
-    </nav>
-  );
+const Navbar = ({ children }) => {
+  return <nav className={Css.Navbar}>{children}</nav>;
 };
 
 export default Navbar;
