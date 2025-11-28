104173662a7749bd748c72551f2c54d6c7b1b131
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-11-29 00:13:06 +0300

    Ana sayfa bileşenine NavLogo, NavList ve NavAuth eklendi; bilgi bölümüne yeni stil ve içerik eklendi.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,22 @@ import Navbar from "../../components/Navbar/Navbar";
 import Css from "./Home.module.css";
 
 // Icons
+import { FaCheck } from "react-icons/fa";
+
+// Icons
 import { GoArrowUpRight } from "react-icons/go";
+import NavLogo from "../../components/NavLogo/NavLogo";
+import NavList from "../../components/NavList/NavList";
+import NavAuth from "../../components/NavAuth/NavAuth";
 
 const Home = () => {
   return (
     <div className={Css.HomePage}>
-      <Navbar />
+      <Navbar>
+        <NavLogo flex={2} />
+        <NavList />
+        <NavAuth />
+      </Navbar>
       <div className={Css.Content}>
         <div className={Css.HeaderSection}>
           <h1 className={Css.Title}>Make Life Easier for the Family:</h1>
@@ -23,7 +33,17 @@ const Home = () => {
             Get Started <GoArrowUpRight size={24} />
           </a>
         </div>
-        <div className={Css.InfoSection}></div>
+        <div className={Css.InfoSection}>
+          <div className={Css.Block}>
+            <div className={Css.IconPlaceholder}>
+              <FaCheck size={20} />
+            </div>
+            <div className={Css.TextContent}>
+              <span className={Css.BlockSubtitle}>Experienced nannies</span>
+              <span className={Css.BlockTitle}>15,000</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
