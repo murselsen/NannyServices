69a7f84cd6255c509b41dfbe70dd8cd44277dce6
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-11-29 00:12:22 +0300

    NavAuth bileşenine esneklik özelliği eklendi ve CSS dosyası oluşturuldu.

--- src/components/NavAuth/NavAuth.jsx
+++ src/components/NavAuth/NavAuth.jsx
@@ -3,12 +3,14 @@ import Css from "./NavAuth.module.css";
 // Component for selecting theme (light/dark)
 import ThemeSelector from "../ThemeSelector/ThemeSelector";
 
-const NavAuth = () => {
+const NavAuth = ({ flex = 1 }) => {
   return (
-    <div className={Css.AuthButtons}>
+    <div className={Css.AuthButtons} style={{ flex: flex }}>
       <button className={Css.LoginButton}>Login</button>
       <button className={Css.SignupButton}>Registration</button>
       <ThemeSelector />
     </div>
   );
 };
+
+export default NavAuth;
