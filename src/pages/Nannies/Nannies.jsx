1145f13dfdd686a3e17b4b90a20c7d840fbe7c7a
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-12-04 00:54:42 +0300

    fix: remove unused useSelector import from Nannies component

--- src/pages/Nannies/Nannies.jsx
+++ src/pages/Nannies/Nannies.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import Css from "./Nannies.module.css";
 
 // Redux
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { fetchNannies } from "../../redux/nannies/thunks.js";
 
 // Components
