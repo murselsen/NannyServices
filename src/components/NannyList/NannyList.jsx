42f6688a5da3368d3426d70b2e2e7d04e7695fa5
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-12-08 03:16:59 +0300

    feat: enhance NannyList component; integrate modal for appointment view and improve item layout

--- src/components/NannyList/NannyList.jsx
+++ src/components/NannyList/NannyList.jsx
@@ -7,10 +7,15 @@ import Css from "./NannyList.module.css";
 // Commponents
 import Filters from "../Filters/Filters";
 import NannyItem from "../NannyItem/NannyItem";
+import { useState } from "react";
+import Modal from "../Modal/Modal";
 
 const NannyList = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMoreView, setIsMoreView] = useState(false);
+  const [isAppointmentView, setIsAppointmentView] = useState(false);
   const { items, isLoading } = useSelector((state) => state.nannies);
-  console.log("Nannies data in NannyList:", items);
+
   return (
     <div className={Css.Container}>
       <Filters />
@@ -20,8 +25,20 @@ const NannyList = () => {
       ) : (
         <ul className={Css.List}>
           {items.map((nanny, index) => (
-            <li key={index} className={Css.Item}>
-              <NannyItem data={nanny} />
+            <li key={index}>
+              <NannyItem
+                index={index}
+                isMoreView={isMoreView}
+                toggleIsMoreView={setIsMoreView}
+                toggleIsAppointmentView={setIsAppointmentView}
+                data={nanny}
+              />
+              <Modal
+                show={isAppointmentView}
+                toggleModal={setIsAppointmentView}
+              >
+                {nanny.name}
+              </Modal>
             </li>
           ))}
         </ul>
