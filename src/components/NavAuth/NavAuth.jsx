fa657d842134bcdbed940d6777d0fa2203235746
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-12-09 12:17:16 +0300

    feat: enhance NavAuth component; add user profile display and logout functionality for authenticated users

--- src/components/NavAuth/NavAuth.jsx
+++ src/components/NavAuth/NavAuth.jsx
@@ -6,11 +6,19 @@ import Modal from "../Modal/Modal";
 import Login from "../Login/Login";
 import Registration from "../Registration/Registration";
 
+// Icons
+import { FaUser } from "react-icons/fa";
+
 // Modules
 import { useState } from "react";
 import { useMediaQuery } from "react-responsive";
 
+// Redux Modules
+import { useDispatch, useSelector } from "react-redux";
+// Redux - Auth Slice
+
 const NavAuth = ({ flex }) => {
+  const { isLoggedIn, user } = useSelector((state) => state.auth);
   const isTabletOrMobile = useMediaQuery({
     maxWidth: 1024,
   });
@@ -20,33 +28,52 @@ const NavAuth = ({ flex }) => {
     setIsModelContentType(type);
   };
   return (
-    <>
-      <div className={Css.AuthButtons} style={{ flex: flex }}>
-        <button
-          className={Css.LoginButton}
-          onClick={() => handleOpenModal("login")}
-        >
-          Login
-        </button>
-        <button
-          className={Css.SignUpButton}
-          onClick={() => handleOpenModal("registration")}
-        >
-          Registration
-        </button>
-        {!isTabletOrMobile && <ThemeSelector />}
-      </div>
-      {isModelContentType === "registration" && (
-        <Modal closeModal={() => setIsModelContentType(false)}>
-          <Registration closeModal={() => setIsModelContentType(false)} />
-        </Modal>
+    <div className={Css.AuthButtons} style={{ flex: flex }}>
+      {isLoggedIn && user ? (
+        <>
+          <div className={Css.Profile}>
+            <div className={Css.Avatar}>
+              <FaUser />
+            </div>
+            <span className={Css.Name}>
+              {user.providerData[0].email.split("@")[0]}
+            </span>
+          </div>
+          <button
+            className={Css.SignUpButton}
+            onClick={() => handleOpenModal("logout")}
+          >
+            Log out
+          </button>
+        </>
+      ) : (
+        <>
+          <button
+            className={Css.LoginButton}
+            onClick={() => handleOpenModal("login")}
+          >
+            Login
+          </button>
+          <button
+            className={Css.SignUpButton}
+            onClick={() => handleOpenModal("registration")}
+          >
+            Registration
+          </button>
+          {!isTabletOrMobile && <ThemeSelector />}
+          {isModelContentType === "registration" && (
+            <Modal closeModal={() => setIsModelContentType(false)}>
+              <Registration closeModal={() => setIsModelContentType(false)} />
+            </Modal>
+          )}
+          {isModelContentType === "login" && (
+            <Modal closeModal={() => setIsModelContentType(false)}>
+              <Login closeModal={() => setIsModelContentType(false)} />
+            </Modal>
+          )}{" "}
+        </>
       )}
-      {isModelContentType === "login" && (
-        <Modal closeModal={() => setIsModelContentType(false)}>
-          <Login closeModal={() => setIsModelContentType(false)} />
-        </Modal>
-      )}
-    </>
+    </div>
   );
 };
 
